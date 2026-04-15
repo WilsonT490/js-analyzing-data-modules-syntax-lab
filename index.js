7013bd2a6d63336c1f99ef22e4cab62efe898ba7
Wilson Mbiuki <wilson.mbiuki@student.moringaschool.com>
2026-04-15 23:36:28 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+require('datejs');
 
+function combineUsers(...args){
+  const combinedObject = {
+    users: []
+  };
+  args.forEach(arg => {
+    if (Array.isArray(arg)) {
+      combinedObject.users.push(...arg);
+    } else {
+      combinedObject.users.push(arg);
+    }
+  });
+  return combinedObject;
+}
 
 
 module.exports = {
